e6b44de4f431d2d4e304d4d60953154098019272
pawgog <p.gogola@interia.pl>
2019-12-13 19:15:40 +0100

    Added sorting and filtering movies funcionality with chips.

--- src/components/App.js
+++ src/components/App.js
@@ -20,18 +20,43 @@ class App extends Component {
       // console.log('movies:', movies)
       this.setState({ movies });
     })
-    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=563bb1cb72253b6be2c32ce80a7a07ef&language=en-US&page=1`)
-    .then(res => {
-      const playing = res.data.results;
-      // console.log('movies:', playing)
-      this.setState({ playing });
-    })
     axios.get(`https://api.themoviedb.org/3/configuration?api_key=563bb1cb72253b6be2c32ce80a7a07ef`)
     .then(res => {
       const images = res.data.images;
       // console.log('movies:', images)
       this.setState({ images });
     })
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=563bb1cb72253b6be2c32ce80a7a07ef&language=en-US&page=1`)
+    .then(res => {
+      const playing = res.data.results;
+      // console.log('movies:', playing)
+      this.setState({ playing });
+      this.sortingMovies(this.state.movies, playing)
+      this.changeImagePath(this.state.images, playing)
+    })
+  }
+
+  sortingMovies(movies, playing) {
+    movies.forEach(val_1 => {
+      playing.forEach(val_2 => {
+        val_2.genre_ids.forEach((genre, index) => {
+        if (val_1.id === genre) {
+          return val_2.genre_ids[index] = val_1
+        }
+      })
+      })
+    })
+    this.setState(playing)
+  }
+
+  changeImagePath(images, playing) {
+    if (Object.entries(images).length !== 0 && images.constructor === Object) {
+      playing.forEach(play => {
+        let playPoster = play.poster_path
+        return play.poster_path = images.base_url + images.poster_sizes[2] + playPoster
+      })      
+      this.setState(playing)      
+    }
   }
 
   render() {
@@ -39,7 +64,6 @@ class App extends Component {
       <div>
        <MoviesList 
           movies={this.state.movies}
-          images={this.state.images}
           playing={this.state.playing}
        ></MoviesList>
       </div>
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
+import {ChipSet, Chip} from '@material/react-chips';
+import '@material/react-chips/dist/chips.css';
 import PropTypes from 'prop-types';
 
 class MoviesList extends Component {
   static propTypes = {
     movies: PropTypes.array.isRequired,
     playing: PropTypes.array.isRequired,
-    images: PropTypes.object.isRequired,
+  }
+
+  state = {
+    query: []
+  }
+
+  filterMovies(filter, moviesList) {
+    let filterMoviesList = []
+    if (filter !== []) {
+      moviesList.forEach(movie => {
+        movie.genre_ids.forEach((genre, index) => {
+          filter.forEach(chip => {
+            if (genre.id === chip) {
+              filterMoviesList.push(movie)
+            }
+          })
+        })
+      })
+      return [...new Set(filterMoviesList)]
+    } else {
+      this.setState({query: filter})
+      return moviesList
+    }
   }
 
   render() {
-    const { movies, images, playing } = this.props
+    const { query } = this.state
+    const { movies, playing } = this.props
+    const moviesList = query.length === 0
+    ? playing
+    : this.filterMovies(this.state.query, playing);
 
     return (
       <div>
-       <p>Test</p>
+        <ChipSet
+          filter
+          handleSelect={(moviesId) => this.setState({query: moviesId})}
+        >
+          {movies.map((movie, index) => (
+            <Chip key={ index } id={ movie.id } label={ movie.name } />          
+          ))}
+        </ChipSet>
+        <div>
+          {moviesList.map((play, index) => (
+            <div key={ index }>
+              <h4>{ play.original_title }</h4>
+                {play.genre_ids.map((genre, index) => (
+                  <span key={ index } id={ genre.id }>{ genre.name } / </span>
+                ))}
+              <img src={ play.poster_path } alt={ play.original_title } />
+            </div>          
+          ))}
+        </div>
       </div>
     );
   }
